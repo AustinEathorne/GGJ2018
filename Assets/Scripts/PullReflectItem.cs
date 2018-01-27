0dbc5f81155c24bc49d1c595e7185ca9e6561c2a
Austin Eathorne <austineathorne@gmail.com>
2018-01-27 01:01:09 -0500

    Pull/Reflect script test

---
+++ Assets/Scripts/PullReflectItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullReflectItem : MonoBehaviour 
+{
+
+	private delegate void PlayerAction();
+
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			PlayerAction playerAction = this.gameObject.tag == "Sun" ?  new PlayerAction(this.Pull) : new PlayerAction(this.Reflect);
+			playerAction();
+		}
+	}
+
+	private void Pull()
+	{
+		Debug.Log("Pull Action");
+	}
+
+	private void Reflect()
+	{
+		Debug.Log("Reflect Action");
+	}
+}
