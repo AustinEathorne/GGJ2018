4f09b1274905b54fd11c1765a9e8e43686bcf2f8
Austin Eathorne <austineathorne@gmail.com>
2018-01-27 18:24:45 -0500

    Base light reflection

---
+++ Assets/Scripts/LightBeam.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBeam : MonoBehaviour {
+
+	[SerializeField]
+	private float maxDistance = 25.0f;
+
+	[SerializeField]
+	private LineRenderer lineRenderer;
+
+	[SerializeField]
+	private float lightSpeed = 5.0f;
+
+	private Vector3 nextPos = Vector3.zero;
+	private Vector3 nextNormal = Vector3.zero;
+
+	private int startIndex = 0;
+
+	private bool hasHitPoint = false;
+
+
+	private IEnumerator Start()
+	{
+		this.StartCoroutine(this.RunBeam());
+		yield return null;
+	}
+
+	private IEnumerator RunBeam()
+	{
+		Debug.Log("Run Beam Routine");
+
+		yield return this.StartCoroutine(this.CastRay());
+		yield return this.StartCoroutine(this.MoveLightBeam());
+		yield return this.StartCoroutine(this.CalculateNextBeam());
+
+		this.hasHitPoint = false;
+		this.StartCoroutine(this.RunBeam());
+	}
+
+	private IEnumerator CastRay()
+	{
+		Ray ray = new Ray(this.transform.position, this.transform.forward);
+		RaycastHit hit;
+
+		Debug.Log("Out");
+
+		while(!hasHitPoint)
+		{
+			Debug.Log("In");
+			Debug.DrawRay(this.transform.position, this.transform.forward, Color.green, 100.0f);
+			if(Physics.Raycast(ray, out hit, 100.0f))
+			{
+				Debug.Log("Waiting");
+
+				if(hit.transform.tag == "ReflectiveSurface")
+				{
+					Debug.Log("Hit Mirror at: " + hit.point);
+					this.hasHitPoint = true;
+
+					this.AddPositionToLine();
+
+					nextPos = hit.point;
+					this.nextNormal = hit.transform.forward;
+				}
+			}
+
+			yield return null;
+		}
+	}
+
+	private IEnumerator MoveLightBeam()
+	{
+		Debug.Log("Move Beam");
+
+		this.lineRenderer.SetPosition(startIndex, this.transform.position);
+		this.lineRenderer.SetPosition(startIndex + 1, this.transform.position);
+
+		float distance = Vector3.Distance(this.transform.position, nextPos);
+		float linelength = 0.0f;
+
+		while(distance > 0.05f)
+		{
+			Debug.Log("Moving");
+
+			this.lineRenderer.SetPosition(startIndex + 1, Vector3.MoveTowards(this.lineRenderer.GetPosition(startIndex + 1), this.nextPos, this.lightSpeed * Time.deltaTime));
+			distance = Vector3.Distance(this.lineRenderer.GetPosition(startIndex + 1), this.nextPos);
+
+			yield return null;
+		}
+		yield return null;
+	}
+
+	private IEnumerator CalculateNextBeam()
+	{
+		Debug.Log("Calculate Next Beam");
+
+		Vector3 dir = (this.lineRenderer.GetPosition(startIndex + 1) - this.lineRenderer.GetPosition(startIndex)).normalized;
+		Vector3 reflection = Vector3.Reflect(dir, this.nextNormal);
+
+		this.transform.rotation = Quaternion.LookRotation(reflection);
+		this.transform.position = this.nextPos;
+
+		yield return null;
+	}
+
+	private void AddPositionToLine()
+	{
+		Vector3[] tempArray = new Vector3[this.lineRenderer.positionCount + 1];
+		this.lineRenderer.GetPositions(tempArray);
+
+		this.lineRenderer.SetPosition(startIndex, this.lineRenderer.GetPosition(this.lineRenderer.positionCount - 1));
+		this.lineRenderer.positionCount += 1;
+
+		for(int i = 0; i < this.lineRenderer.positionCount; i++)
+		{
+			this.lineRenderer.SetPosition(i, tempArray[i]);
+		}
+
+		this.startIndex += 1;
+	}
+
+}
--- Assets/Scripts/LightReflector.cs
+++ Assets/Scripts/LightReflector.cs
@@ -11,38 +11,110 @@ public class LightReflector : MonoBehaviour
 	private Transform targetTransform;
 
 	[SerializeField]
-	private float lightStep = 0.1f;
+	private float lightLength = 10.0f;
+
+	[SerializeField]
+	private float lightSpeed = 0.1f;
 
 	[SerializeField]
 	private float stoppingDistance = 0.25f;
 
+	private bool hasFired = false;
+
+	[SerializeField]
+	private bool hasNextTarget = false;
+
+	//debug
+	[SerializeField]
+	private bool isFirstSource = false;
+	private Vector3 initialLightDirection = Vector3.forward;
 
 	public void Start()
 	{
-		this.StartCoroutine(this.StartReflection(45));
+		if(isFirstSource)
+		{
+			this.initialLightDirection = (this.targetTransform.position - this.transform.position).normalized;
+			this.StartCoroutine(this.StartReflection(this.initialLightDirection));
+		}
 	}
 
-	public IEnumerator StartReflection(float angle)
+	public IEnumerator StartReflection(Vector3 lightDirection)
 	{
-		
-		this.lineRenderer.SetPosition(0, Vector3.zero);
-		this.lineRenderer.SetPosition(1, Vector3.zero);
-		//this.transform.localRotation = Quaternion.Euler(new Vector3(0.0f, -angle, 0.0f));
+		if(this.hasFired)
+			yield break;
 
+		//Debug.Log("Initial Direction: " + lightDirection);
 
+		this.hasFired = true;
 
+		// Setup line renderer
+		this.lineRenderer.SetPosition(0, this.transform.position);
+		this.lineRenderer.SetPosition(1, this.transform.position);
 		this.lineRenderer.enabled = true;
 
-		//Vector3 targetDirection = (this.transform.TransformPoint(this.lineRenderer.GetPosition(1)) - this.targetTransform.position).normalized;
-		float targetDistance = Vector3.Distance(this.transform.TransformVector(this.lineRenderer.GetPosition(1)), this.targetTransform.localPosition);
+		// Setup line collider
 
-		while(targetDistance >= this.stoppingDistance)
+		BoxCollider lineCollider = new GameObject("LineCollider").AddComponent<BoxCollider>();
+		lineCollider.transform.parent = this.lineRenderer.gameObject.transform;
+		lineCollider.tag = "Line";
+		lineCollider.isTrigger = true;
+		float lineWidth = this.lineRenderer.endWidth;
+		float lineLength = Vector3.Distance(this.lineRenderer.GetPosition(0), this.lineRenderer.GetPosition(1));
+		lineCollider.size = new Vector3(lineLength, lineWidth, this.lineRenderer.endWidth);
+		Vector3 midPoint = (this.lineRenderer.GetPosition(0) + this.lineRenderer.GetPosition(1)) * 0.5f;
+		lineCollider.transform.position = midPoint;
+		float angle = Mathf.Atan2((this.lineRenderer.GetPosition(1).z - this.lineRenderer.GetPosition(0).z), (this.lineRenderer.GetPosition(1).x - this.lineRenderer.GetPosition(0).x));
+		angle *= Mathf.Rad2Deg;
+		angle *= -1;
+		lineCollider.transform.localRotation = Quaternion.Euler(0.0f, angle, 0.0f);
+
+		float targetDistance = Vector3.Distance(this.lineRenderer.GetPosition(1), this.targetTransform.position);
+		float lightStep = targetDistance / this.lightSpeed;
+		lineLength = 0.0f;
+		//Physics.IgnoreCollision(this.GetComponent<Collider>(), );
+
+		while(targetDistance >= this.stoppingDistance && lineLength <= this.lightLength)
 		{
-			targetDistance = Vector3.Distance(this.transform.TransformVector(this.lineRenderer.GetPosition(1)), this.targetTransform.position);
-
+		/*
+			targetDistance = Vector3.Distance(this.lineRenderer.GetPosition(1), this.targetTransform.position);
 			this.lineRenderer.SetPosition(1, Vector3.MoveTowards(this.transform.TransformVector(this.lineRenderer.GetPosition(1)), this.targetTransform.localPosition, this.lightStep * Time.deltaTime));
 
+			lineLength = Vector3.Distance(this.lineRenderer.GetPosition(0), this.lineRenderer.GetPosition(1));
+			lineCollider.size = new Vector3(lineLength, lineWidth, this.lineRenderer.endWidth);
+
+			midPoint = (this.lineRenderer.GetPosition(0) + this.lineRenderer.GetPosition(1)) * 0.5f;
+			lineCollider.transform.position = midPoint;
+
+			angle = Mathf.Atan2((this.lineRenderer.GetPosition(1).z - this.lineRenderer.GetPosition(0).z), (this.lineRenderer.GetPosition(1).x - this.lineRenderer.GetPosition(0).x));
+			angle *= Mathf.Rad2Deg;
+			angle *= -1;
+			lineCollider.transform.localRotation = Quaternion.Euler(0.0f, angle, 0.0f);
+
+			lineLength = Vector3.Distance(this.lineRenderer.GetPosition(0), this.lineRenderer.GetPosition(1));
+			targetDistance = Vector3.Distance(this.lineRenderer.GetPosition(1), lightDirection * this.lightLength);
+			this.lineRenderer.SetPosition(1, Vector3.MoveTowards(this.lineRenderer.GetPosition(1), transform.TransformDirection(lightDirection * this.lightLength), lightStep * Time.deltaTime));//Vector3.MoveTowards(this.lineRenderer.GetPosition(1), this.targetTransform.localPosition, this.lightStep * Time.deltaTime));
+			*/
+			/*
+			lineLength = Vector3.Distance(this.lineRenderer.GetPosition(0), this.lineRenderer.GetPosition(1));
+			lineCollider.size = new Vector3(lineLength, lineWidth, this.lineRenderer.endWidth);
+
+			midPoint = (this.lineRenderer.GetPosition(0) + this.lineRenderer.GetPosition(1)) * 0.5f;
+			lineCollider.transform.position = midPoint;
+
+			angle = Mathf.Atan2((this.lineRenderer.GetPosition(1).z - this.lineRenderer.GetPosition(0).z), (this.lineRenderer.GetPosition(1).x - this.lineRenderer.GetPosition(0).x));
+			angle *= Mathf.Rad2Deg;
+			angle *= -1;
+			lineCollider.transform.localRotation = Quaternion.Euler(0.0f, angle, 0.0f);
+			*/
 			yield return null;
+		}
+
+		if(this.hasNextTarget)
+		{
+			Vector3 dir = (this.lineRenderer.GetPosition(1) - this.lineRenderer.GetPosition(0));
+			Vector3 reflection = Vector3.Reflect(dir, transform.TransformDirection(this.targetTransform.forward)).normalized;
+			Debug.Log("reflection: " + reflection.ToString());
+			this.targetTransform.GetComponent<LightReflector>().StartCoroutine(this.targetTransform.GetComponent<LightReflector>().StartReflection(reflection));
 		}
 
 		yield return null;
