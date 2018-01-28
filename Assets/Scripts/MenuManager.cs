ee8bcd2d86afa1aea4551857dbc13a76cce6d710
Austin Eathorne <austineathorne@gmail.com>
2018-01-28 08:23:05 -0500

    main menu base w/ controller navigation

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour 
 {
+	[SerializeField]
+	private Button creditsButton;
+
+	[SerializeField]
+	private Button creditsCloseButton;
+
 	public void QuitGame()
 	{
 		Application.Quit();
@@ -14,5 +21,14 @@ public class MenuManager : MonoBehaviour
 	{
 		SceneManager.LoadScene(1);
 	}
-	
+
+	public void OpenCreditsPanel()
+	{
+		this.creditsCloseButton.Select();
+	}
+
+	public void CloseCreditsPanel()
+	{
+		this.creditsButton.Select();
+	}
 }
