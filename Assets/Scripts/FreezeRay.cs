c7578f3ab23994e87d99663fb96b7695ae640398
macky90078 <mackenzie.w115@gmail.com>
2018-01-27 21:18:49 -0500

    Mack's Shit!

---
+++ Assets/Scripts/FreezeRay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeRay : MonoBehaviour {
+
+    private GameObject m_hitEnemy = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UseFreeze();
+            Debug.Log("FREEZE");
+        }
+    }
+
+    private void UseFreeze()
+    {
+        RayCastCheck(transform.forward);
+    }
+
+    private void RayCastCheck(Vector3 rayDirection)
+    {
+        Ray ray = new Ray(transform.position, rayDirection);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 10f))
+        {
+            if (hit.transform.tag == "enemy")
+            {
+                Debug.Log("hit!!");
+                hit.transform.gameObject.GetComponent<TestAI>().m_isFrozen = true;
+            }
+        }
+
+    }
+}
